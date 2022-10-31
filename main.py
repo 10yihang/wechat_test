3a1a4cdd2b5873ae0d69740e3725679188932841
10yihang <115096615+10yihang@users.noreply.github.com>
2022-11-01 01:30:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 {
 # 公众号配置
 # 公众号appId
-"app_id": "###########",
+"app_id": "wxb52b6acf3c5c2db7",
 # 公众号appSecret
-"app_secret": "###########",
+"app_secret": "4d3ca1b7ddf7e9445d798523919cf35a",
 # 模板消息id
-"template_id": "###########",
+"template_id": "	7s5jCgphRKU5DPXl66VYtvtU0oLj-coxnH37C-icShs",
 # 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-"user": ["###########"],
+"user": ["ok-D15iJh7YNgZ_QxGZ_t7Hza3vM"],
  
 # 信息配置
 # 和风天气apikey
-"weather_key": "###########",
+"weather_key": "97e68df9aef04cb585769fa31ac23fb3",
 # 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
-"region": "###########",
+"region": "柯桥区",
 # 生日1，修改名字为对应需要显示的名字，如果生日为农历，在最前面加上r即可
-"birthday1": {"name": "###########", "birthday": "2004-04-08"},
+"birthday1": {"name": "余倩倩", "birthday": "2004-02-08"},
 # 生日2
 "birthday2": {"name": "东阁", "birthday": "2018-08-15"},
 # 在一起的日子，格式同上，暂不不支持农历
